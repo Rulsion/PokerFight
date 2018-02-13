d3e8dc9908fcecb954083afccdd2f2d2b3c8eadb
Rulsion <786172053@qq.com>
2018-02-13 17:30:34 +0800

    123

--- src/pokerFight/log4j/Loggers.java
+++ src/pokerFight/log4j/Loggers.java
@@ -2,10 +2,19 @@ package pokerFight.log4j;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-public class Loggers {
+public class Loggers{
 	private static Logger logger = LogManager.getLogger("Loggers");
 	
-	public static void loggerOut(char level,String msg) {
+	public static void loggerOut(String level,String msg) {
+		if (level.equals("I")) {
+			logger.info(msg);
+		} else if (level.equals("W")) {
+
+		} else if (level.equals("E")) {
+
+		} else if (level.equals("M")) {
+
+		}
 		
 	}
 
